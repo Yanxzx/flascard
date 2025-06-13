afd1ee52cd8213eaedab2ebd71c078aa4b4a0eae
Yanxzx <matheus.freitas.silva07@escola.pr.gov.br>
2025-06-13 11:57:42 +0000

    Pending changes exported from your codespace

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Esportes',
+    'Qual o esporte mais popular do mundo?',
+    'Futebol'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual o jogo mais jogado de todos os tempos?',
+    'Minecraft'
+)
+
+criaCartao(
+    'Filosofia',
+    'Quem é o filosofo que disse:"Só sei que nada sei"?',
+    'Socrates'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha',
+    'Madrid'
+)
